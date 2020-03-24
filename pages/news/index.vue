4c860b50bb7cd63dcc5d6591d424bc9d0652623c
zainras <zain95394@gmail.com>
2020-03-25 01:09:33 +0700

    remove api error on berita

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <h1>Berita</h1>
-    <ul v-for="article in news" :key="article">
-      <li>{{ article.title.rendered }}</li>
-    </ul>
   </div>
 </template>
 
@@ -12,11 +9,6 @@ export default {
   data: {
     news: [],
   },
-  async asyncData({ $axios }) {
-    const data = await $axios.$get('https://kawalcovid19.id/_next/data/eRnviEofSpR2YFgL9WIKA/index.json')
-    let news = data.pageProps.information
-    return { news: news }
-  }
 }
 </script>
 
