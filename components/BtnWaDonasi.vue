3f6b4d0522beb86c6f2ecee6c0ec63a2ae6a7953
zainras <zain95394@gmail.com>
2020-03-27 14:47:22 +0700

    href donasi

--- components/BtnWaDonasi.vue
+++ components/BtnWaDonasi.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    href="#"
+    :href="linkDonasi"
     target="_blank"
     class="px-10 py-4 rounded-lg text-white border-2 border-solid border-white hover:bg-brand-green-light"
   >
@@ -9,8 +9,20 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      nomorTujuan: '6281335790535',
+      textPesan: 'Assalamuaalikum%20Warohamtullah%20Wabarokatuh.%0D%0A%0D%0A'
+                  + 'Saya ingin berdonasi membantu tenaga dokter yang berjuang melawan Virus Covid19 di fasilitas pelayanan kesehatan di Ponorogo.'
+    }
+  },
+  computed: {
+    linkDonasi() {
+      return `https://web.whatsapp.com/send?phone=${this.nomorTujuan}&text=${this.textPesan}`
+    }
+  }
+};
 </script>
-
 <style>
 </style>
