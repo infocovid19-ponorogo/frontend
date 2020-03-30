2c3d325d4c1c031120507ebf2b3c37ae47f92b24
zainras <zain95394@gmail.com>
2020-03-30 20:44:26 +0700

    add pernyataan resmi

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,11 +3,15 @@
     <div class="container lg:mx-auto">
       <div class="flex">
         <div class="max-w-sm flex items-center flex-shrink-0 text-white mr-6 lg:p-0 pl-6 py-2">
-          <img class="lg:fill-current h-10 lg:h-16 lg:w-full" src="~/assets/icon.svg" alt="infocovid19ponorogo.id">
+          <img
+            class="lg:fill-current h-10 lg:h-16 lg:w-full"
+            src="~/assets/icon.svg"
+            alt="infocovid19ponorogo.id"
+          />
         </div>
         <div class="lg:relative fixed bottom-0 w-full bg-blue-400 h-14 py-2 lg:pt-4">
           <ul class="flex flex-row lg:justify-start justify-between text-center ml-3">
-            <li class="flex-1 lg:flex-none ml-3 mr-3">
+            <li class="flex-1 lg:flex-none mr-2">
               <nuxt-link to="/" :class="activeLink('/')">
                 <div class="flex justify-center">
                   <svg
@@ -24,14 +28,11 @@
                     />
                   </svg>
                 </div>
-                <span class="lg:text-base text-xs lg:text-white pb-1">Beranda</span>
+                <span class="lg:text-base text-super-xs lg:text-white pb-1">Beranda</span>
               </nuxt-link>
             </li>
-            <li class="flex-1 lg:flex-none mr-3">
-              <nuxt-link
-                to="/info"
-                :class="activeLink('/info')"
-              >
+            <li class="flex-1 lg:flex-none mr-2">
+              <nuxt-link to="/info" :class="activeLink('/info')">
                 <div class="flex justify-center">
                   <svg
                     class="lg:hidden w-5 fill-current"
@@ -47,14 +48,11 @@
                     />
                   </svg>
                 </div>
-                <span class="lg:text-base text-xs lg:text-white pb-1">Informasi</span>
+                <span class="lg:text-base text-super-xs lg:text-white pb-1">Informasi</span>
               </nuxt-link>
             </li>
-            <li class="flex-1 lg:flex-none mr-3">
-              <nuxt-link
-                to="/about-us"
-                :class="activeLink('/about-us')"
-              >
+            <li class="flex-1 lg:flex-none mr-2">
+              <nuxt-link to="/about-us" :class="activeLink('/about-us')">
                 <div class="flex justify-center">
                   <svg
                     class="lg:hidden w-5 fill-current"
@@ -70,7 +68,47 @@
                     />
                   </svg>
                 </div>
-                <span class="lg:text-base text-xs lg:text-white pb-1">Tentang Kami</span>
+                <span class="lg:text-base text-super-xs lg:text-white pb-1">Tentang Kami</span>
+              </nuxt-link>
+            </li>
+            <li class="flex-1 lg:flex-none mr-2">
+              <nuxt-link to="/policy-privacy" :class="activeLink('/policy-privacy')">
+                <div class="flex justify-center">
+                  <svg
+                    class="lg:hidden w-5 fill-current"
+                    v-bind:class="isActiveLink('/policy-privacy') ? 'text-white' : 'text-black'"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="15"
+                    height="15"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M7 10V7a5 5 0 1 1 10 0v3h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h2zm2 0h6V7a3 3 0 0 0-6 0v3zm-4 2v8h14v-8H5zm7 2a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1z"
+                    />
+                  </svg>
+                </div>
+                <span class="lg:text-base text-super-xs lg:text-white pb-1">Kebijakan Privasi</span>
+              </nuxt-link>
+            </li>
+            <li class="flex-1 lg:flex-none mr-2">
+              <nuxt-link to="/pernyataan-resmi" :class="activeLink('/pernyataan-resmi')">
+                <div class="flex justify-center">
+                  <svg
+                    class="lg:hidden w-5 fill-current"
+                    v-bind:class="isActiveLink('/pernyataan-resmi') ? 'text-white' : 'text-black'"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="15"
+                    height="15"
+                  >
+                    <path
+                      class="heroicon-ui"
+                      d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9z"
+                    />
+                  </svg>
+                </div>
+                <span class="lg:text-base text-super-xs lg:text-white pb-1">Pernyataan Resmi</span>
               </nuxt-link>
             </li>
           </ul>
@@ -84,16 +122,26 @@
 export default {
   methods: {
     activeLink(link) {
-      let isActive = this.isActiveLink(link)
-      return (isActive) ? 'flex flex-col text-white lg:border-b-2 lg:border-white lg:pb-2 lg:mr-5' : 'flex flex-col lg:border-b-2 lg:border-gray-400 lg:border-none lg:pb-2 lg:mr-5'
+      let isActive = this.isActiveLink(link);
+      return isActive
+        ? "flex flex-col text-white lg:border-b-2 lg:border-white lg:pb-2 lg:mr-5"
+        : "flex flex-col lg:border-b-2 lg:border-gray-400 lg:border-none lg:pb-2 lg:mr-5";
     },
     isActiveLink(link) {
-      let firtspath = this.$route.path.split('/')[1]
-      return link == '/' + firtspath
+      let firtspath = this.$route.path.split("/")[1];
+      return link == "/" + firtspath;
     }
   }
 };
 </script>
 
 <style>
+.text-super-xs {
+  font-size: 8.1px;
+}
+@media (min-width: 1024px) {
+  .text-super-xs {
+      font-size: 1rem;
+  }
+}
 </style>
---
+++ pages/pernyataan-resmi/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="w-full mt-5">
+    <div class="container mx-auto px-5">
+      <div id="article-content">
+        <h1 class="text-3xl font-bold">Pernyataan Resmi</h1>
+        <p>Menyikapi adanya beberapa masukan terkait visualisasi data dalam bentuk peta untuk tidak ditampilkan dan juga adanya kesalahan teknis dari sisi programming kami. Kami dari infocovid19ponorogo.id mengambil langkah untuk tidak menampilkan peta persebaran yang ada.</p>
+
+        <p>Senin, 30 Maret 2020, kami mendatangi Kominfo Ponorogo terkait penyesuaian data dan juga permintaan maaf terkait kesalahan penyajian data. Dari pihak Kominfo Ponorogo telah memberikan saran agar lebih berhati-hati sekaligus pernyataan agar data yang disajikan benar-benar valid dan mengikuti data yang bersumber di website <a href="https://ponorogo.go.id/corona">https://ponorogo.go.id/corona</a>.</p>
+
+        <p>Jika diluar sana ada yang memberitakan bahwa kami menyebarkan hoax itu sungguh sangat keliru, karena tidak ada hal yang kami sengaja di dalam kekeliruan kami. Tentu kami bisa mengajukan keberatan atas berita yang beredar tentang tuduhan itu,tetapi kami percaya <strong>kebermanfaatan website ini jauh lebih banyak ketimbang isu negatifnya.</strong></p>
+
+        <p>Sebenarnya dalam website kami sudah ada kebijakan privasi terkait konten yang ada,bisa dibaca pada <a href="https://infocovid19ponorogo.id/policy-privacy/">https://infocovid19ponorogo.id/policy-privacy/</a>  dan website ini juga merujuk pada lisensi internasional <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode.id">https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode.id</a>.Jika ada kekeliruan atau kesalahan data yang kami tampilkan bisa dikoreksi dan kami akan meralat sebagai bentuk pertanggung jawaban kami.</p>
+
+        <p>Dengan berat hati kami menutup akses peta persebaran di website kami,terima kasih atas perhatian pengunjung dan pembaca yang telah mengakses website kami.</p>
+
+        <p>Kami akan terus berusaha untuk tidak berhenti menginfokan sesuatu yang bermanfaat kepada masyarakat khususnya Masyarakat Ponorogo.Sekali lagi terima kasih atas support kritik dan sarannya.</p>
+
+        <p>Demikian klarifikasi dari kami,terima kasih.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  layout: "home",
+  async asyncData({ $axios }) {
+    const dtIndo = await $axios.$get(
+      "https://kawalcovid19.harippe.id/api/summary"
+    );
+    return { dtIndo };
+  }
+};
+</script>
+
+<style>
+#article-content {
+  overflow-wrap: break-word;
+}
+#article-content h1 {
+  margin-bottom: 18px;
+  font-size: 36px;
+  line-height: 36px;
+  font-weight: 600;
+}
+#article-content h2 {
+  margin-bottom: 16px;
+  font-size: 32px;
+  line-height: 36px;
+  font-weight: 600;
+}
+#article-content h3 {
+  margin-bottom: 14px;
+  font-size: 28px;
+  line-height: 36px;
+  font-weight: 600;
+}
+#article-content h4 {
+  margin-bottom: 12px;
+  font-size: 24px;
+  line-height: 36px;
+  font-weight: 600;
+}
+#article-content h5 {
+  margin-bottom: 10px;
+  font-size: 20px;
+  line-height: 36px;
+  font-weight: 600;
+}
+#article-content h6 {
+  margin-bottom: 8px;
+  font-size: 14px;
+  line-height: 36px;
+  font-weight: 600;
+}
+#article-content p {
+  line-height: 24px;
+  margin-bottom: 16px;
+}
+#article-content ul {
+  display: block;
+  list-style-type: none;
+  margin: 12px 12px 16px 16px;
+  padding: 0px;
+}
+#article-content ul li {
+  font-size: 16px;
+  line-height: 24px;
+  margin-bottom: 12px;
+}
+#article-content ul > li::before {
+  content: "–";
+  display: inline-block;
+  color: rgb(102, 107, 115);
+  position: absolute;
+  margin-left: -15px;
+}
+#article-content .wp-block-table {
+  overflow-x: auto;
+}
+#article-content a {
+  color: #63b3ed;
+}
+</style>
